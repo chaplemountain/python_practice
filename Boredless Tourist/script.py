e83863706d97eda1af995085e79185831e2ce601
chaplemountain <ryanr4269@gmail.com>
2020-09-03 19:38:49 -0400

    added attractions function, tested

--- Boredless Tourist/script.py
+++ Boredless Tourist/script.py
@@ -1,5 +1,6 @@
 destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "Sao Paulo, Brazil", "Cairo, Egypt"]
 test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+attractions = [[] for attraction in destinations]
 
 #pull destination index
 def get_destination_index(destination):
@@ -14,4 +15,29 @@ def get_traveler_location(traveler):
 
 #test traveler location function
 test_destination_index = get_traveler_location(test_traveler)
-print(test_destination_index)
+#print(test_destination_index)
+
+#add attractions
+def add_attraction(destination, attraction):
+    try:
+        destination_index = get_destination_index(destination)
+    except ValueError:
+        print("Destination does not exist!")
+        return
+    attractions_for_destination = attractions[destination_index]
+    attractions_for_destination.append(attraction)
+    return
+
+#test add_attractions
+add_attraction("Los Angeles, USA", ['Venice Beach', ['beach']])
+add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
+add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
+add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historcical site"]])
+add_attraction("Shanghai, China", ["Yuz Museum", ["art", "museum"]])
+add_attraction("Shanghai, China", ["Oriental Pearl Tower", ["skyscraper", "viewing deck"]])
+add_attraction("Los Angeles, USA", ["LACMA", ["art", "museum"]])
+add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
+add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
+add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
+add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
+print(attractions)
