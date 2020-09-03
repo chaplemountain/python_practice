86612b7006b45cbfba70953757f3ac7d1ed96370
chaplemountain <ryanr4269@gmail.com>
2020-09-03 09:05:13 -0400

    added destination indexing function

--- Boredless Tourist/script.py
+++ Boredless Tourist/script.py
@@ -1,2 +1,9 @@
 destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "Sao Paulo, Brazil", "Cairo, Egypt"]
 test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+
+#pull destination index
+def get_destination_index(destination):
+   destination_index = destinations.index(destination)
+   return destination_index
+
+print(get_destination_index("Hyderabad, India"))
