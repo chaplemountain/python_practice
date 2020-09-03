e250a040d89a88dd377a9072c852eeb40765139f
chaplemountain <ryanr4269@gmail.com>
2020-09-03 19:14:02 -0400

    added traveler location function

--- Boredless Tourist/script.py
+++ Boredless Tourist/script.py
@@ -6,4 +6,12 @@ def get_destination_index(destination):
    destination_index = destinations.index(destination)
    return destination_index
 
-print(get_destination_index("Hyderabad, India"))
+#get traveler location
+def get_traveler_location(traveler):
+    traveler_destination = traveler[1]
+    traveler_destination_index = get_destination_index(traveler_destination)
+    return traveler_destination_index
+
+#test traveler location function
+test_destination_index = get_traveler_location(test_traveler)
+print(test_destination_index)
