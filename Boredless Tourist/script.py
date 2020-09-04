9601f83d59ac2c67e4bdae9deb4c932297f956a7
chaplemountain <ryanr4269@gmail.com>
2020-09-04 07:40:15 -0400

    added attractions finder based on interests

--- Boredless Tourist/script.py
+++ Boredless Tourist/script.py
@@ -13,20 +13,34 @@ def get_traveler_location(traveler):
     traveler_destination_index = get_destination_index(traveler_destination)
     return traveler_destination_index
 
-#test traveler location function
-test_destination_index = get_traveler_location(test_traveler)
-#print(test_destination_index)
-
 #add attractions
 def add_attraction(destination, attraction):
     try:
         destination_index = get_destination_index(destination)
     except ValueError:
-        print("Destination does not exist!")
+        #print("Destination does not exist!")
         return
     attractions_for_destination = attractions[destination_index]
     attractions_for_destination.append(attraction)
     return
+
+#find attractions based off interests
+def find_attractions(destination, interests):
+    destination_index = get_destination_index(destination)
+    attractions_in_city = attractions[destination_index]
+    attractions_with_interest = []
+    for attraction in attractions_in_city:
+        possible_attraction = attraction
+        attraction_tags = possible_attraction[1]
+        for interest in interests:
+            for attraction in attraction_tags:
+                if interest == attraction:
+                    attractions_with_interest.append(possible_attraction[0])
+    return attractions_with_interest
+
+#test traveler location function
+test_destination_index = get_traveler_location(test_traveler)
+#print(test_destination_index)
 
 #test add_attractions
 add_attraction("Los Angeles, USA", ['Venice Beach', ['beach']])
@@ -40,4 +54,8 @@ add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
 add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
 add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
 add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
-print(attractions)
+#print(attractions)
+
+#test attractions with interests
+la_arts = find_attractions("Los Angeles, USA", ['art'])
+print(la_arts)
